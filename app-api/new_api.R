da551ded0dd375a0d83ff74064ae810d82820eb0
Gina727 <153985861+Gina727@users.noreply.github.com>
2024-04-26 16:05:28 +0800

    Update new_api.R

--- app-api/new_api.R
+++ app-api/new_api.R
@@ -48,8 +48,7 @@ check_files <- function(req, res){
 #* @param link:str
 #* @post /user_url_download
 #* This api is used to download the file uploaded by the client to the same directory of this R script on the server.
-function(link, key, req, res){
-  key <<- req$HTTP_KEY
+function(link, sys.user_id, req, res){
   destfile <<- paste0("./", sys.user_id, ".RDS")
   download.file(link, destfile)
   destfile
