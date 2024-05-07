116065518c2a612f862a54e2349ef45417daa13f
Gina727 <153985861+Gina727@users.noreply.github.com>
2024-05-07 19:40:08 +0800

    Update new_api.R

--- app-api/new_api.R
+++ app-api/new_api.R
@@ -51,27 +51,28 @@ check_files <- function(req, res){
 function(link, sys.user_id, req, res){
   sys.user_id <<- sys.user_id
   destfile <<- paste0("./", sys.user_id, ".RDS")
-  download.file(link, destfile)
+  download.file(link, file.path("rds", destfile))
   destfile
 }
 
 #* @post /qcplot
+#* @serializer unboxedJSON
 #* This api is used to analyze the percentage of mitochondria gene, distribution of number of gene and cells, and plot a violin graph for quality control.
 qcplot <- function(req, res){
-  seurat_obj <- readRDS(paste0(sys.user_id, ".RDS"))
+  seurat_obj <- readRDS(file.path("rds", paste0(sys.user_id, ".RDS")))
   if(is.null(seurat_obj)){
     stop("No Seurat object uploaded", call. = FALSE)
   }
   seurat_obj[["percent.mt"]] <- PercentageFeatureSet(seurat_obj, pattern = "^MT-")
   
   file_name <- paste0(sys.user_id, "-qcplot", '.RDS')
-  SaveSeuratRds(seurat_obj, file = file_name)
+  SaveSeuratRds(seurat_obj, file = file.path("rds", file_name))
 
   d <- VlnPlot(seurat_obj, features = c("nFeature_RNA", "percent.mt"), ncol = 2, pt.size = 0.1)
   graph_name = paste0(sys.user_id,"-vlnplot",".png")
   ggsave(filename = file.path("images", graph_name), plot = d, width = 10, height = 10, dpi = 300)
 
-  return(graph_name)
+  list(status = "SUCCESS", code = "200", output = list(type = "image", content = paste0("http://scrna.m2mda.com/images/", graph_name)))
 }
 # Handler for loading picture, response resource type what
 # Define response type
@@ -82,22 +83,22 @@ qcplot <- function(req, res){
 #* @get /qc
 #* This api is used for quality control, selecting desired range of number of genes and cells, and a maximum threshold for mitochondria genes
 qc <- function(min.features, max.features, max.mtpercent){
-  seurat_obj <- readRDS(paste0(sys.user_id, "-qcplot", ".RDS"))
+  seurat_obj <- readRDS(file.path("rds", paste0(sys.user_id, "-qcplot", ".RDS")))
   min.features <- as.numeric(min.features)
   max.features <- as.numeric(max.features)
   max.mtpercent <- as.numeric(max.mtpercent)
   seurat_obj <- subset(seurat_obj, subset = nFeature_RNA > min.features & nFeature_RNA < max.features & percent.mt < max.mtpercent)
   
   file_name <- paste0(sys.user_id, "-qc", '.RDS')
-  SaveSeuratRds(seurat_obj, file = file_name)
+  SaveSeuratRds(seurat_obj, file = file.path("rds", file_name))
 }
 
-#* @serializer png
+#* @serializer unboxedJSON
 #* @post /norm_pca
 #* This api is used for normalization and pca reduction so that the expression matrix can be normalized and find top variable genes. PCA reduction is then used to reduce multidimensional matrix to a 2D graph.
 norm_pca <- function(scaling_factor, num_hvgs, norm_method, hvg_method, res){
 
-  seurat_obj <- readRDS(paste0(sys.user_id, "-qc", ".RDS"))
+  seurat_obj <- readRDS(file.path("rds", paste0(sys.user_id, "-qc", ".RDS")))
   scaling_factor <- as.numeric(scaling_factor)
   num_hvgs <- as.numeric(num_hvgs)
 
@@ -111,18 +112,21 @@ norm_pca <- function(scaling_factor, num_hvgs, norm_method, hvg_method, res){
   # Run PCA
   seurat_obj <- RunPCA(seurat_obj, features = VariableFeatures(object = seurat_obj))
 
-  d = ElbowPlot(object = seurat_obj, ndims = 40)
-  print(d)
+  d <- ElbowPlot(object = seurat_obj, ndims = 40)
+  graph_name = paste0(sys.user_id, "-elbowplot",".png")
+  ggsave(filename = file.path("images", graph_name), plot = d, width = 10, height = 10, dpi = 300)
 
   file_name <- paste0(sys.user_id,"-norm_pca", '.RDS')
-  SaveSeuratRds(seurat_obj, file = file_name)
+  SaveSeuratRds(seurat_obj, file = file.path("rds", file_name))
+
+  list(status = "SUCCESS", code = "200", output = list(type = "image", content = paste0("http://scrna.m2mda.com/images/", graph_name)))
 }
 
 #* @post /clustering_umap
 #* This api is used for clustering (with UMAP visualization) the Seurat object with client's parameter input, dim , which is chosen from the previous elbow plot, and resolution, the extent of how many clusters will be generated (high resolution: more clusters)
 clustering_umap <- function(dim, resolution, res){
   
-  seurat_obj <- readRDS(paste0(sys.user_id,"-norm_pca",".RDS"))
+  seurat_obj <- readRDS(file.path("rds", paste0(sys.user_id,"-norm_pca",".RDS")))
 
   dim <- as.numeric(dim)
   resolution <- as.numeric(resolution)
@@ -132,14 +136,13 @@ clustering_umap <- function(dim, resolution, res){
   seurat_obj <- RunUMAP(seurat_obj, dims = 1:dim)
 
   file_name <- paste0(sys.user_id, "-clustering_umap", '.RDS')
-  SaveSeuratRds(seurat_obj, file = file_name)
+  SaveSeuratRds(seurat_obj, file = file.path("rds", file_name))
 }
 
-#* @serializer png
 #* @post /clustering_tsne
 #* This api is used for clustering (with t-SNE visualization) the Seurat object with client's parameter input, dim , which is chosen from the previous elbow plot, and resolution, the extent of how many clusters will be generated (high resolution: more clusters)
 clustering_tsne <- function(dims, resolution, res){
-  seurat_obj <- readRDS(paste0(sys.user_id, "-norm_pca", ".RDS"))
+  seurat_obj <- readRDS(file.path("rds", paste0(sys.user_id, "-norm_pca", ".RDS")))
 
   dims <- as.numeric(dims)
   resolution <- as.numeric(resolution)
@@ -149,21 +152,16 @@ clustering_tsne <- function(dims, resolution, res){
   seurat_obj <- RunTSNE(seurat_obj, dims = 1:dims)
 
   file_name <- paste0(sys.user_id, "-clustering_tsne", '.RDS')
-  SaveSeuratRds(seurat_obj, file = file_name)
-
-  t = TSNEPlot(seurat_obj)
-  print(t)
-
-  res$setHeader("X-API-KEY", key)
+  SaveSeuratRds(seurat_obj, file = file.path("rds", file_name))
 }
 
 #* annotation_sctype_umap
-#* @serializer png
+#* @serializer unboxedJSON
 #* @post /annotation_sctype_umap
 #* This api is used for annotating the clustered object with scType algorithm, calculating the sctype score with positive and negative marker gene sets to classify the cell type. UMAP is used for visualization.
 annotation_sctype_umap <- function(tissue, res){
 
-  seurat_obj <- readRDS(paste0(sys.user_id, "-clustering_umap", ".RDS"))
+  seurat_obj <- readRDS(file.path("rds", paste0(sys.user_id, "-clustering_umap", ".RDS")))
 
   # prepare gene sets
     gs_list = gene_sets_prepare(db_, tissue)
@@ -187,19 +185,22 @@ annotation_sctype_umap <- function(tissue, res){
     }
 
     file_name <- paste0(sys.user_id, "-annotation_umap", '.RDS')
-    SaveSeuratRds(seurat_obj, file = file_name)
+    SaveSeuratRds(seurat_obj, file = file.path("rds", file_name))
 
     d <- DimPlot(seurat_obj, reduction = "umap", label = TRUE, repel = TRUE, group.by = 'customclassif')+ggtitle(tissue)
-    print(d)
+    graph_name = paste0(sys.user_id,"-annotation_umap",".png")
+    ggsave(filename = file.path("images", graph_name), plot = d, width = 10, height = 10, dpi = 300)
+
+    list(status = "SUCCESS", code = "200", output = list(type = "image", content = paste0("http://scrna.m2mda.com/images/", graph_name)))
 }
 
 #* annotation_sctype_tsne
-#* @serializer png
+#* @serializer unboxedJSON
 #* @post /annotation_sctype_tsne
 #* This api is used for annotating the clustered object with scType algorithm, calculating the sctype score with positive and negative marker gene sets to classify the cell type. t-SNE is used for visualization.
 annotation_sctype_tsne <- function(tissue, res){
 
-  seurat_obj <- readRDS(paste0(sys.user_id, "-clustering_tsne", ".RDS"))
+  seurat_obj <- readRDS(file.path("rds", paste0(sys.user_id, "-clustering_tsne", ".RDS")))
 
   # prepare gene sets
     gs_list = gene_sets_prepare(db_, tissue)
@@ -223,10 +224,13 @@ annotation_sctype_tsne <- function(tissue, res){
       seurat_obj@meta.data$customclassif[seurat_obj@meta.data$seurat_clusters == j] = as.character(cl_type$type[1])
     }
     file_name <- paste0(sys.user_id, "-annotation_tsne", '.RDS')
-    SaveSeuratRds(seurat_obj, file = file_name)
+    SaveSeuratRds(seurat_obj, file = file.path("rds", file_name))
 
-    b = TSNEPlot(seurat_obj, group.by = 'customclassif')+ggtitle(tissue)
-    print(b)
+    b <- TSNEPlot(seurat_obj, group.by = 'customclassif')+ggtitle(tissue)
+    graph_name = paste0(sys.user_id,"-annotation_tsne",".png")
+    ggsave(filename = file.path("images", graph_name), plot = d, width = 10, height = 10, dpi = 300)
+
+    list(status = "SUCCESS", code = "200", output = list(type = "image", content = paste0("http://scrna.m2mda.com/images/", graph_name)))
 }
 
 # download
